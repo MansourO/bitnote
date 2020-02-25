27eee53a4595956e791108ba5bb059f9e1098c2d
omansou <osama.mansour@aarons.com>
2020-02-25 10:21:39 -0500

    Add Firebase for Persistance

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ // Your web app's Firebase configuration
+  const config = {
+    apiKey: "AIzaSyAUxSQQyvQo4MejPf-ZMFkrtKscgYt70MM",
+    authDomain: "bitnote-49190.firebaseapp.com",
+    databaseURL: "https://bitnote-49190.firebaseio.com",
+    projectId: "bitnote-49190",
+    storageBucket: "bitnote-49190.appspot.com",
+    messagingSenderId: "176955867904",
+    appId: "1:176955867904:web:36e519145d5dcc29eb69e5",
+    measurementId: "G-FH3Y2YH0LZ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
+  firebase.analytics();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
