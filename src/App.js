0c2be78b2ebd07dbebea8a33fd2c3890c02c2dc9
omansou <osama.mansour@aarons.com>
2020-02-25 14:20:30 -0500

    Initial Add Notes Functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import * as firebase from 'firebase';
 
+import Header from './components/Header';
+import NotesForm from "./components/NotesForm";
+
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <main>
+        <NotesForm />
+      </main>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = props => (
+  <header className="header">
+    <h3 className="title">
+        BitNote
+    </h3>
+  </header>  
+)
+
+export default Header;
